930c0e334e797dc04357b5a5b39fa9aa43da1c7a
Ives van Hoorne <ives.v.h@gmail.com>
2020-07-11 16:11:38 +0200

    Update JSDOM and create script to automate it

---
+++ standalone-packages/browser-jsdom/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const browserify = require('browserify');
+const cp = require('child_process');
+
+const jsdompkg = JSON.parse(fs.readFileSync('./node_modules/jsdom/package.json').toString())
+const jsdomloc = path.resolve(path.join('./node_modules/jsdom', jsdompkg.main));
+
+const outLoc = `out/jsdom-${jsdompkg.version}.js`
+const outMinLoc = `out/jsdom-${jsdompkg.version}.min.js`
+cp.execSync(`node_modules/.bin/browserify -s JSDOM ${jsdomloc} > ${outLoc}`);
+cp.execSync(`node_modules/.bin/terser ${outLoc} > ${outMinLoc}`);
